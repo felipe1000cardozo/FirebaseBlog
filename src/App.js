3c314f8e478a826df8da1a57acac90ea4203906b
felipe1000cardozo <37625101+felipe1000cardozo@users.noreply.github.com>
2020-02-17 19:04:48 -0300

    Created routes and global styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { Fragment } from "react";
+import GlobalStyle from "./styles/global";
+import Routes from "./routes";
 
 function App() {
-  return <div>hello</div>;
+  return (
+    <Fragment>
+      <GlobalStyle />
+      <Routes />
+    </Fragment>
+  );
 }
 
 export default App;
