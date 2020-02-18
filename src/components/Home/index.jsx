d4ee9493021b84a4f9804304968efcddd0bea0cf
felipe1000cardozo <37625101+felipe1000cardozo@users.noreply.github.com>
2020-02-18 15:24:47 -0300

    acess DB and render posts

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,10 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import firebase from "../../firebase";
 
 const Home = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    firebase.app.ref("posts").once("value", snapshot => {
+      let newPosts = [];
+
+      snapshot.forEach(childItem => {
+        newPosts.push({
+          key: childItem.key,
+          titulo: childItem.val().titulo,
+          imagem: childItem.val().imagem,
+          descricao: childItem.val().descricao,
+          autor: childItem.val().autor
+        });
+      });
+
+      setPosts(newPosts);
+    });
+  }, []);
+
   return (
-    <div>
-      <h1>home</h1>
-    </div>
+    <section>
+      {posts.map(post => {
+        return (
+          <article key={post.key}>
+            <header>
+              <div>
+                <strong>{post.titulo}</strong>
+                <span>Autor: {post.autor}</span>
+              </div>
+            </header>
+            <img src={post.imagem} alt="Capa do post" />
+            <footer>
+              <p>{post.descricao}</p>
+            </footer>
+          </article>
+        );
+      })}
+    </section>
   );
 };
 
